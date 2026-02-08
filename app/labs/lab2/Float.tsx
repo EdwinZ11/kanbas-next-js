7d3ec1c50eada89d10ca3aa6457f863e8f0aa5f2
Edwin Zhou <90789780+EdwinZ11@users.noreply.github.com>
2026-02-08 13:18:52 -0500

    completed entirety of 2.1

---
+++ app/labs/lab2/Float.tsx
@@ -0,0 +1,38 @@
+export default function Float() {
+    return (
+        <div>
+          <div id="wd-float-divs">
+           <h2>Float</h2>
+           <div>
+             <img className="wd-float-right"
+               src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="starship"/>
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam faucibus elit sed lacus accumsan facilisis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean sed purus at tortor pretium pharetra sit amet et nibh. Integer viverra malesuada enim vel luctus. Fusce convallis nulla id magna volutpat, a convallis purus tempor. Quisque elit ligula, aliquam vitae felis nec, ultricies cursus odio. Nullam et feugiat leo. Sed sit amet varius magna, a pretium libero. Ut pellentesque at nunc ac accumsan. Nulla a imperdiet leo. Duis purus urna, dapibus nec mollis vel, eleifend sed erat.
+             <img className="wd-float-left"
+               src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="starship"/>
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam faucibus elit sed lacus accumsan facilisis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean sed purus at tortor pretium pharetra sit amet et nibh. Integer viverra malesuada enim vel luctus. Fusce convallis nulla id magna volutpat, a convallis purus tempor. Quisque elit ligula, aliquam vitae felis nec, ultricies cursus odio. Nullam et feugiat leo. Sed sit amet varius magna, a pretium libero. Ut pellentesque at nunc ac accumsan. Nulla a imperdiet leo. Duis purus urna, dapibus nec mollis vel, eleifend sed erat.
+             <img className="wd-float-right"
+               src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="starship"/>
+             Nam vel lorem ultrices, dictum augue ut, tincidunt risus. Morbi eleifend mollis velit non egestas. Praesent elementum vel odio vel vulputate. Etiam est elit, elementum vel tempor at, tincidunt a quam. Vestibulum dictum nisl vitae nulla rhoncus molestie. Aenean eu varius augue. Praesent varius interdum congue. Integer eget tellus in tortor placerat egestas. Ut suscipit, urna quis efficitur lacinia, lacus quam gravida purus, eget condimentum tellus orci at purus. Duis pulvinar commodo mi, ac rhoncus elit malesuada non. Quisque massa purus, laoreet vel accumsan sed, dignissim vel dui. Aliquam eu commodo orci. Donec sit amet nunc ac quam aliquet finibus. Ut id massa ac erat venenatis venenatis in vitae nisl. Maecenas congue efficitur diam tincidunt efficitur. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+             <img className="wd-float-left"
+               src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="starship"/>
+             Donec et pretium libero. Suspendisse sem magna, euismod sed aliquam et, sollicitudin sed massa. Phasellus massa ex, mollis sed diam et, tempor viverra ligula. Curabitur sed interdum ligula. Duis sed ornare justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris vitae sapien in ipsum vehicula lacinia sit amet ac nulla. Ut lobortis neque ex, ut faucibus lectus finibus sit amet. Proin vehicula dapibus quam, ut eleifend nibh ultricies ac. Quisque vulputate, arcu at elementum mattis, metus leo volutpat magna, ac maximus mi felis et nunc. Sed et odio at neque porta dapibus. Morbi malesuada dapibus maximus.
+             <div className="wd-float-done"></div>
+           </div>
+          </div>
+          <div id="wd-float-divs">
+          <h2>Float</h2>
+          <div>
+            <div className="wd-float-left wd-dimension-portrait wd-bg-color-yellow">
+              Yellow </div>
+            <div className="wd-float-left wd-dimension-portrait wd-bg-color-blue wd-fg-color-white">
+              Blue </div>
+            <div className="wd-float-left wd-dimension-portrait wd-bg-color-red">
+              Red </div>
+            <img className="wd-float-right"
+              src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"/>
+            <div className="wd-float-done"></div>
+          </div>
+          </div>
+        </div>
+    )
+}
